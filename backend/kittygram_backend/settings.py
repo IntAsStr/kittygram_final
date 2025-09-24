b566437b8547d3343083dd0ad522f9124c61ebad
Nikolay-S <grueger@yandex.ru>
2025-09-24 16:45:41 +0300

    review_1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -18,8 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework.authtoken',
     'rest_framework',
+    'rest_framework.authtoken',
     'djoser',
     'cats.apps.CatsConfig',
 ]
